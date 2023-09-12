8b88fcb17b8acc831676366e44b762effa144342
elan-sk <elan-sk@hotmail.com>
2023-09-12 08:38:58 -0500

    Clases 3

---
+++ src/TodoItem.js
@@ -0,0 +1,11 @@
+function TodoItem() {
+    return (
+        <li>
+            <span>v</span>
+            <p>Llorar con la llorona </p>
+            <span>x</span>
+        </li>
+    );
+}
+
+export { TodoItem };
